ce61cca1c2d552781f990e6e9fb0d0cb57804e81
Hayami Tatsuya <tatsuya.hayami@proton.me>
2025-08-29 23:55:00 +0200

    remove typo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,8 +12,6 @@
 		href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap"
 		rel="stylesheet"
 	/>
-
-	<link rel="icon" href={favicon} />
 </svelte:head>
 
 <nav
